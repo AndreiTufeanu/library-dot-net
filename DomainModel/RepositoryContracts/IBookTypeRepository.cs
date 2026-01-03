f2525c11151dfb180ac8fa02864afe36949b0d93
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-03 20:55:03 +0200

    feat: add initial IBookTypeRepository interface

---
+++ DomainModel/RepositoryContracts/IBookTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using DomainModel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.RepositoryContracts
+{
+    public interface IBookTypeRepository
+    {
+        BookType GetById(Guid id);
+        IEnumerable<BookType> GetAll();
+        void Add(BookType entity);
+        void Update(BookType entity);
+        void Delete(Guid id);
+        bool Exists(Guid id);
+        void SaveChanges();
+    }
+}
