8e46b43b8c5f396d358915e2de4ee39dacf592b1
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-03 21:04:16 +0200

    feat: add service configuration in console project

---
+++ LibraryAppConsole/StartUp/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+using ServiceLayer.ServiceExtensions;
+using Infrastructure.ServiceExtensions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.ApplicationContext;
+
+namespace LibraryAppConsole.StartUp
+{
+    public class Startup
+    {
+        public static IServiceProvider ConfigureServices()
+        {
+            // Configure Serilog to log to console
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console(
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+
+            var services = new ServiceCollection();
+
+            // Add logging
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddSerilog(dispose: true);
+            });
+
+            // Add Infrastructure services
+            services.AddInfrastructureServices();
+
+            // Add ServiceLayer services
+            services.AddServiceLayerServices();
+
+            // Add DbContext
+            services.AddScoped<LibraryContext>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
