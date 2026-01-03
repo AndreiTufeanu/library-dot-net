50828e3f61f6c7d5f0f1349b6f7847c4b68eafc9
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-03 21:05:31 +0200

    chore: update config files for multiple projects

--- LibraryAppConsole/Program.cs
+++ LibraryAppConsole/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using ServiceLayer.ServiceContracts;
+using ServiceLayer.Services;
 using System.Collections.Generic;
 using System.Linq;
+using DomainModel.Entities;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using LibraryAppConsole.StartUp;
 
 namespace LibraryAppConsole
 {
