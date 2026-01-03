2994dc09e7c65e7ab21451ee43c62a3806889556
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-03 20:57:56 +0200

    feat: add initial IBookTypeServie interface

---
+++ ServiceLayer/ServiceContracts/IBookTypeService.cs
@@ -0,0 +1,29 @@
+﻿using DomainModel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ServiceContracts
+{
+    public interface IBookTypeService
+    {
+        ServiceResult<BookType> CreateBookType(BookType bookType);
+        ServiceResult<BookType> GetBookTypeById(Guid id);
+        ServiceResult<IEnumerable<BookType>> GetAllBookTypes();
+        ServiceResult<bool> UpdateBookType(BookType bookType);
+        ServiceResult<bool> DeleteBookType(Guid id);
+        ServiceResult<bool> BookTypeExists(Guid id);
+    }
+
+    public class ServiceResult<T>
+    {
+        public bool Success { get; set; }
+        public T Data { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public static ServiceResult<T> SuccessResult(T data) => new ServiceResult<T> { Success = true, Data = data };
+        public static ServiceResult<T> FailureResult(string error) => new ServiceResult<T> { Success = false, ErrorMessage = error };
+    }
+}
