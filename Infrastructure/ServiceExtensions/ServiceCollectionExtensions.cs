8d30e44bbd3c69200b0b31ada76eecdb6b1a12e4
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-03 20:59:52 +0200

    feat: add service collection extension methods for DI

---
+++ Infrastructure/ServiceExtensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using DomainModel.RepositoryContracts;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ServiceExtensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IBookTypeRepository, BookTypeRepository>();
+            return services;
+        }
+    }
+}
---
+++ ServiceLayer/ServiceExtensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceLayer.Validators;
+using System;
+using DomainModel.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ServiceExtensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddServiceLayerServices(this IServiceCollection services)
+        {
+            services.AddTransient<IValidator<BookType>, BookTypeValidator>();
+
+            services.AddScoped<ServiceContracts.IBookTypeService, Services.BookTypeService>();
+
+            return services;
+        }
+    }
+}
