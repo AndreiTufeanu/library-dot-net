48e77cb418f762d4682a2e4b5bfc203e57456f32
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-03 20:44:17 +0200

    feat: add book type validator

---
+++ ServiceLayer/Validators/BookTypeValidator.cs
@@ -0,0 +1,18 @@
+﻿using DomainModel.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Validators
+{
+    public class BookTypeValidator : AbstractValidator<BookType>
+    {
+        public BookTypeValidator()
+        {
+            
+        }
+    }
+}
