01d4759cc9f0e21faa9cdbfd65d2833c0ad98385
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-05 00:30:57 +0200

    feat: update Configuration.cs to add default values for constants

--- Infrastructure/Migrations/Configuration.cs
+++ Infrastructure/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 ﻿namespace Infrastructure.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using DomainModel.Entities;
+    using ServiceLayer.Helpers;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Infrastructure.ApplicationContext.LibraryContext>
     {
@@ -18,6 +21,26 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+            // Use ConfigurationConstants to get all default settings
+            var defaultSettings = new List<ConfigurationSetting>();
+
+            foreach (var (key, value, dataType, category, description) in ConfigurationConstants.GetAllDefaults())
+            {
+                defaultSettings.Add(new ConfigurationSetting
+                {
+                    Key = key,
+                    Value = value,
+                    DataType = dataType,
+                    Category = category,
+                    Description = description,
+                    LastModified = DateTime.UtcNow
+                });
+            }
+
+            foreach (var setting in defaultSettings)
+            {
+                context.ConfigurationSettings.AddOrUpdate(s => s.Key, setting);
+            }
         }
     }
 }
