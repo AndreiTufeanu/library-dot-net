0a9291f2b111f30300fd8c7fd9bd1e8433b4d618
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-04 15:36:48 +0200

    feat: update IBookTypeService

--- ServiceLayer/ServiceContracts/IBookTypeService.cs
+++ ServiceLayer/ServiceContracts/IBookTypeService.cs
@@ -7,23 +7,7 @@ using System.Threading.Tasks;
 
 namespace ServiceLayer.ServiceContracts
 {
-    public interface IBookTypeService
+    public interface IBookTypeService : IService<BookType>
     {
-        ServiceResult<BookType> CreateBookType(BookType bookType);
-        ServiceResult<BookType> GetBookTypeById(Guid id);
-        ServiceResult<IEnumerable<BookType>> GetAllBookTypes();
-        ServiceResult<bool> UpdateBookType(BookType bookType);
-        ServiceResult<bool> DeleteBookType(Guid id);
-        ServiceResult<bool> BookTypeExists(Guid id);
-    }
-
-    public class ServiceResult<T>
-    {
-        public bool Success { get; set; }
-        public T Data { get; set; }
-        public string ErrorMessage { get; set; }
-
-        public static ServiceResult<T> SuccessResult(T data) => new ServiceResult<T> { Success = true, Data = data };
-        public static ServiceResult<T> FailureResult(string error) => new ServiceResult<T> { Success = false, ErrorMessage = error };
     }
 }
