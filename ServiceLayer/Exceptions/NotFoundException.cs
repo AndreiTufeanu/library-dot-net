6e3a2e45e44d262f048e20ad2407b4edc9a7821a
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-04 15:33:55 +0200

    feat: add custom exceptions

---
+++ ServiceLayer/Exceptions/NotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(string entityName, object id)
+            : base($"{entityName} with ID '{id}' not found.")
+        {
+        }
+    }
+}
