eb8af743fb41052725f9ae4df0a626f5b78e1248
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-04 15:34:31 +0200

    feat: add ValidationHelper class

---
+++ ServiceLayer/Helpers/ValidationHelper.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Helpers
+{
+    public static class ValidationHelper
+    {
+        /// <summary>
+        /// Validates an object using Data Annotations and FluentValidation
+        /// Throws ValidationException if validation fails
+        /// </summary>
+        public static void Validate<T>(T obj, IValidator<T> validator = null)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+
+            var errors = new List<string>();
+
+            // Data Annotations validation
+            var validationContext = new ValidationContext(obj);
+            var validationResults = new List<ValidationResult>();
+
+            if (!Validator.TryValidateObject(obj, validationContext, validationResults, true))
+            {
+                errors.AddRange(validationResults
+                    .Select(vr => vr.ErrorMessage)
+                    .Where(error => !string.IsNullOrWhiteSpace(error)));
+            }
+
+            // FluentValidation (if validator provided)
+            if (validator != null)
+            {
+                var fluentResult = validator.Validate(obj);
+                if (!fluentResult.IsValid)
+                {
+                    errors.AddRange(fluentResult.Errors
+                        .Select(e => e.ErrorMessage)
+                        .Where(error => !string.IsNullOrWhiteSpace(error)));
+                }
+            }
+
+            // Throw if any errors
+            if (errors.Any())
+            {
+                throw new Exceptions.ValidationException(errors);
+            }
+        }
+    }
+}
