913f062e53b5fa2082d60d69561c59332980eaae
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-05 00:26:54 +0200

    feat: add IConfigurationService interface

---
+++ ServiceLayer/ServiceContracts/IConfigurationService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ServiceContracts
+{
+    public interface IConfigurationService
+    {
+        // Book Constants
+        Task<int> GetMaxDomainsPerBookAsync();
+        Task<int> GetDefaultMaxDomainsPerBookAsync();
+
+        // Reader Constants
+        Task<int> GetMaxBooksInPeriodAsync();
+        Task<TimeSpan> GetBorrowingPeriodAsync();
+        Task<int> GetMaxBooksPerBorrowingAsync();
+        Task<int> GetMaxBooksSameDomainAsync();
+        Task<TimeSpan> GetSameDomainTimeLimitAsync();
+        Task<int> GetMaxOvertimeSumAsync();
+        Task<TimeSpan> GetSameBookDelayAsync();
+        Task<int> GetMaxBooksPerDayAsync();
+
+        // Librarian Constants
+        Task<int> GetMaxBooksLentPerDayAsync();
+
+        // Method to refresh cache for a specific key
+        Task RefreshSettingAsync(string key);
+    }
+}
