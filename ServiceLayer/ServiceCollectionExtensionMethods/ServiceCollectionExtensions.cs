98581703b0116f5b55f7a67d97bafd57f8604722
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-06 01:20:36 +0200

    feat: registed the new validators in the service collection extension method

--- ServiceLayer/ServiceCollectionExtensionMethods/ServiceCollectionExtensions.cs
+++ ServiceLayer/ServiceCollectionExtensionMethods/ServiceCollectionExtensions.cs
@@ -12,7 +12,12 @@ namespace ServiceLayer.ServiceCollectionExtensionMethods
         public static IServiceCollection AddServiceLayerServices(this IServiceCollection services)
         {
             // Register validators
+            services.AddTransient<IValidator<Book>, BookValidator>();
             services.AddTransient<IValidator<BookType>, BookTypeValidator>();
+            services.AddTransient<IValidator<Borrowing>, BorrowingValidator>();
+            services.AddTransient<IValidator<Domain>, DomainValidator>();
+            services.AddTransient<IValidator<Edition>, EditionValidator>();
+            services.AddTransient<IValidator<Reader>, ReaderValidator>();
 
             // Register services
             services.AddScoped<IBookTypeService, BookTypeService>();
