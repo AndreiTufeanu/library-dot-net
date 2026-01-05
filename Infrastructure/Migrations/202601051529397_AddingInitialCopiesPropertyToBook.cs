c62660b8c469af1d0f3b27e74de9ae53abb3f914
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-05 17:33:17 +0200

    feat: add migration to add InitialCopies property to Book model

---
+++ Infrastructure/Migrations/202601051529397_AddingInitialCopiesPropertyToBook.cs
@@ -0,0 +1,18 @@
+﻿namespace Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddingInitialCopiesPropertyToBook : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Books", "InitialCopies", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Books", "InitialCopies");
+        }
+    }
+}
