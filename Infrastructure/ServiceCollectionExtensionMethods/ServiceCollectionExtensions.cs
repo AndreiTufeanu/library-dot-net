85a5644dbdb1516d274ecd8665b08b31db99cbac
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-06 01:49:42 +0200

    feat: register all repositories for DI

--- Infrastructure/ServiceCollectionExtensionMethods/ServiceCollectionExtensions.cs
+++ Infrastructure/ServiceCollectionExtensionMethods/ServiceCollectionExtensions.cs
@@ -13,8 +13,17 @@ namespace Infrastructure.ServiceCollectionExtensionMethods
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
         {
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
+            services.AddScoped<IBookCopyRepository, BookCopyRepository>();
             services.AddScoped<IBookTypeRepository, BookTypeRepository>();
+            services.AddScoped<IBorrowingRepository, BorrowingRepository>();
             services.AddScoped<IConfigurationSettingRepository, ConfigurationSettingRepository>();
+            services.AddScoped<IDomainRepository, DomainRepository>();
+            services.AddScoped<IEditionRepository, EditionRepository>();
+            services.AddScoped<ILibrarianRepository, LibrarianRepository>();
+            services.AddScoped<IReaderRepository, ReaderRepository>();
+
             services.AddMemoryCache();
             return services;
         }
