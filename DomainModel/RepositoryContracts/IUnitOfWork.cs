203b7dda35a179a3af1ebccab906d9018958d7bf
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-06 22:31:36 +0200

    feat: add IUnitOfWork interface

---
+++ DomainModel/RepositoryContracts/IUnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.RepositoryContracts
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        // Repository properties
+        IAuthorRepository Authors { get; }
+        IBookRepository Books { get; }
+        IBookCopyRepository BookCopies { get; }
+        IBookTypeRepository BookTypes { get; }
+        IDomainRepository Domains { get; }
+        IEditionRepository Editions { get; }
+        IReaderRepository Readers { get; }
+        ILibrarianRepository Librarians { get; }
+        IBorrowingRepository Borrowings { get; }
+
+        // Transaction management
+        Task BeginTransactionAsync();
+        Task CommitAsync();
+        Task RollbackAsync();
+
+        // Save changes
+        Task<int> SaveChangesAsync();
+    }
+}
