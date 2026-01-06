f8767efe3fcfc1a234fe97db134678b7486278f5
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-06 23:06:01 +0200

    feat: register newly added services and helper services for DI

--- ServiceLayer/ServiceCollectionExtensionMethods/ServiceCollectionExtensions.cs
+++ ServiceLayer/ServiceCollectionExtensionMethods/ServiceCollectionExtensions.cs
@@ -2,7 +2,9 @@
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using ServiceLayer.ServiceContracts;
+using ServiceLayer.ServiceContracts.HelperServiceContracts;
 using ServiceLayer.Services;
+using ServiceLayer.Services.HelperServices;
 using ServiceLayer.Validators;
 
 namespace ServiceLayer.ServiceCollectionExtensionMethods
@@ -12,16 +14,27 @@ namespace ServiceLayer.ServiceCollectionExtensionMethods
         public static IServiceCollection AddServiceLayerServices(this IServiceCollection services)
         {
             // Register validators
-            services.AddTransient<IValidator<Book>, BookValidator>();
             services.AddTransient<IValidator<BookType>, BookTypeValidator>();
+            services.AddTransient<IValidator<Book>, BookValidator>();
             services.AddTransient<IValidator<Borrowing>, BorrowingValidator>();
             services.AddTransient<IValidator<Domain>, DomainValidator>();
             services.AddTransient<IValidator<Edition>, EditionValidator>();
             services.AddTransient<IValidator<Reader>, ReaderValidator>();
 
-            // Register services
+            // Register helper services
+            services.AddScoped<IBookHelperService, BookHelperService>();
+            services.AddScoped<IBorrowingHelperService, BorrowingHelperService>();
+
+            // Register main services
+            services.AddScoped<IAuthorService, AuthorService>();
+            services.AddScoped<IBookService, BookService>();
+            services.AddScoped<IBookCopyService, BookCopyService>();
             services.AddScoped<IBookTypeService, BookTypeService>();
             services.AddScoped<IConfigurationSettingService, ConfigurationSettingService>();
+            services.AddScoped<IDomainService, DomainService>();
+            services.AddScoped<IEditionService, EditionService>();
+            services.AddScoped<IReaderService, ReaderService>();
+            services.AddScoped<ILibrarianService, LibrarianService>();
 
             return services;
         }
