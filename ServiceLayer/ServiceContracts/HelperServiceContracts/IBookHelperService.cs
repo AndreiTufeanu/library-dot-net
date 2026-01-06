4b3170273a28892ed68d2dfbd70146cbae568f82
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-06 22:45:21 +0200

    feat: add interfaces for helper services

---
+++ ServiceLayer/ServiceContracts/HelperServiceContracts/IBookHelperService.cs
@@ -0,0 +1,14 @@
+﻿using DomainModel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ServiceContracts.HelperServiceContracts
+{
+    public interface IBookHelperService
+    {
+        Task ValidateMaxDomainsPerBookAsync(ICollection<Domain> domains);
+    }
+}
---
+++ ServiceLayer/ServiceContracts/HelperServiceContracts/IBorrowingHelperService.cs
@@ -0,0 +1,30 @@
+﻿using DomainModel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ServiceContracts.HelperServiceContracts
+{
+    public interface IBorrowingHelperService
+    {
+        // Validation methods
+        Task ValidateMaxBooksPerBorrowingAsync(Borrowing borrowing);
+        Task ValidateMaxBooksInPeriodAsync(Borrowing borrowing);
+        Task ValidateMaxBooksSameDomainAsync(Borrowing borrowing);
+        Task ValidateMaxOvertimeSumDaysAsync(Borrowing borrowing);
+        Task ValidateSameBookDelayAsync(Borrowing borrowing);
+        Task ValidateMaxBooksPerDayAsync(Borrowing borrowing);
+        Task ValidateLibrarianLendingLimitAsync(Borrowing borrowing);
+
+        // Utility methods
+        Task<bool> IsReaderAlsoLibrarianAsync(Guid readerId);
+        Task<int> GetBooksBorrowedInPeriodAsync(Guid readerId, DateTime startDate, DateTime endDate);
+        Task<int> GetBooksBorrowedFromDomainInPeriodAsync(Guid readerId, Guid domainId, DateTime startDate);
+        Task<int> GetTotalExtensionDaysInPeriodAsync(Guid readerId, DateTime startDate);
+        Task<DateTime?> GetLastBorrowDateForBookAsync(Guid readerId, Guid bookId);
+        Task<int> GetBooksBorrowedOnDateAsync(Guid readerId, DateTime date);
+        Task<int> GetBooksLentByLibrarianOnDateAsync(Guid librarianId, DateTime date);
+    }
+}
