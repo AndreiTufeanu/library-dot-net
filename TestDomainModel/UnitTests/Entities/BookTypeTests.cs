272fc3de69b5746eddb17e269638ceec2269385c
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-07 23:50:30 +0200

    feat: add test class for BookType

---
+++ TestDomainModel/UnitTests/Entities/BookTypeTests.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using DomainModel.Entities;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDomainModel.UnitTests.Entities
+{
+    [TestClass]
+    public class BookTypeTests
+    {
+
+    }
+}
