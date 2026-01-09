0500ebaf62a225a8a5bcad7e8ee4b4dd4556d7c1
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-09 18:13:27 +0200

    feat: add new migration to mark domain name as unique

---
+++ Infrastructure/Migrations/202601091603492_MakingDomainNameUnique.cs
@@ -0,0 +1,18 @@
+﻿namespace Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MakingDomainNameUnique : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.Domains", "Name", unique: true);
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Domains", new[] { "Name" });
+        }
+    }
+}
