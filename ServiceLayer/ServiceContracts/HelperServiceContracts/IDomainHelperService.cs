b11d2ef780bca20aebb9c4391538b089bb1991b2
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-11 18:09:35 +0200

    feat add DomainHelperService and its interface

---
+++ ServiceLayer/ServiceContracts/HelperServiceContracts/IDomainHelperService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ServiceContracts.HelperServiceContracts
+{
+    /// <summary>
+    /// Provides helper methods for domain hierarchy operations.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This service encapsulates operations related to domain hierarchies, including
+    /// traversing ancestor and descendant relationships. It is designed to be used
+    /// by other helper services that need domain hierarchy information.
+    /// </para>
+    /// </remarks>
+    public interface IDomainHelperService
+    {
+        /// <summary>
+        /// Recursively adds all ancestor domain IDs for the specified domain to the collection.
+        /// </summary>
+        /// <param name="domainId">The domain identifier to start from.</param>
+        /// <param name="domainIds">The collection to add ancestor domain IDs to.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        /// <remarks>
+        /// This method traverses up the domain hierarchy chain, adding all parent,
+        /// grandparent, etc. domain IDs to the provided collection.
+        /// </remarks>
+        Task PopulateAncestorDomainIdsAsync(Guid domainId, HashSet<Guid> domainIds);
+
+        /// <summary>
+        /// Recursively adds all descendant domain IDs for the specified domain to the collection.
+        /// </summary>
+        /// <param name="domainId">The domain identifier to start from.</param>
+        /// <param name="domainIds">The collection to add descendant domain IDs to.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        /// <remarks>
+        /// This method traverses down the domain hierarchy chain, adding all child,
+        /// grandchild, etc. domain IDs to the provided collection. It ensures no
+        /// duplicates are added.
+        /// </remarks>
+        Task PopulateDescendantDomainIdsAsync(Guid domainId, HashSet<Guid> domainIds);
+    }
+}
---
+++ ServiceLayer/Services/HelperServices/DomainHelperService.cs
@@ -0,0 +1,43 @@
+﻿using DomainModel.RepositoryContracts;
+using ServiceLayer.ServiceContracts.HelperServiceContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.Services.HelperServices
+{
+    public class DomainHelperService : IDomainHelperService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DomainHelperService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        }
+
+        public async Task PopulateAncestorDomainIdsAsync(Guid domainId, HashSet<Guid> domainIds)
+        {
+            var domain = await _unitOfWork.Domains.GetByIdAsync(domainId);
+            if (domain?.ParentDomain != null)
+            {
+                domainIds.Add(domain.ParentDomain.Id);
+                await PopulateAncestorDomainIdsAsync(domain.ParentDomain.Id, domainIds);
+            }
+        }
+
+        public async Task PopulateDescendantDomainIdsAsync(Guid domainId, HashSet<Guid> domainIds)
+        {
+            var subdomains = await _unitOfWork.Domains.GetSubdomainsAsync(domainId);
+            foreach (var subdomain in subdomains)
+            {
+                if (!domainIds.Contains(subdomain.Id))
+                {
+                    domainIds.Add(subdomain.Id);
+                    await PopulateDescendantDomainIdsAsync(subdomain.Id, domainIds);
+                }
+            }
+        }
+    }
+}
