75bbeeabd48a05e1cb98557edb0099f859ba0b96
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2026-01-11 13:36:57 +0200

    test: add test for ConfigurationConstants

---
+++ TestServiceLayer/Helpers/ConfigurationConstantsTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLayer.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestServiceLayer.Helpers
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class ConfigurationConstantsTests
+    {
+
+        [TestMethod]
+        public void GetAllDefaults_ShouldReturnCorrectNumberOfItems()
+        {
+            // Act
+            var defaults = ConfigurationConstants.GetAllDefaults();
+
+            // Assert
+            defaults.Should().HaveCount(12);
+        }
+    }
+}
